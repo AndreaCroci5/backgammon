8e591f808c8968303ec227f2a355d9278d2596ca
Andrea <andrea5.croci@mail.polimi.it>
2024-09-21 14:34:24 +0200

    Add feature backgammon board initialization in Circuit.java

--- src/main/java/it/croci/games/backgammon/model/Circuit.java
+++ src/main/java/it/croci/games/backgammon/model/Circuit.java
@@ -4,5 +4,55 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Circuit {
+    //ATTRIBUTES
+    private Map<Integer, Integer> boardCircuit;
 
+
+    //CONSTRUCTOR
+
+    public Circuit(Map<Integer, Integer> boardCircuit) {
+        this.boardCircuit = boardCircuit;
+    }
+
+    public Circuit() {
+        this.boardCircuit = new HashMap<>();
+        //Circuit initialization
+        initCircuit(this.boardCircuit);
+    }
+
+    //GETTER AND SETTER
+
+
+    public Map<Integer, Integer> getBoardCircuit() {
+        return boardCircuit;
+    }
+
+    public void setBoardCircuit(Map<Integer, Integer> boardCircuit) {
+        this.boardCircuit = boardCircuit;
+    }
+
+
+
+    //PUBLIC METHODS
+    public void initCircuit(Map<Integer, Integer> boardCircuit) {
+        for (int i=0; i<24; i++) {
+            switch (i){
+                case 0:
+                    boardCircuit.put(i, 2);
+                    break;
+                case 11:
+                    boardCircuit.put(i, 5);
+                    break;
+                case 16:
+                    boardCircuit.put(i, 3);
+                    break;
+                case 18:
+                    boardCircuit.put(i, 5);
+                    break;
+                default:
+                    boardCircuit.put(i, 0);
+                    break;
+            }
+        }
+    }
 }
