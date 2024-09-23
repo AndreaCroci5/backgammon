49ba36305bdd75bfe5a87780cb03f5a616a51521
Andrea <andrea5.croci@mail.polimi.it>
2024-09-23 08:52:11 +0200

    Fix initialization feature by making the board shared

--- src/main/java/it/croci/games/backgammon/model/Circuit.java
+++ src/main/java/it/croci/games/backgammon/model/Circuit.java
@@ -1,58 +1,96 @@
 package it.croci.games.backgammon.model;
 
+import it.croci.games.backgammon.model.exceptions.MoveException;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Circuit {
     //ATTRIBUTES
-    private Map<Integer, Integer> boardCircuit;
+    private Map<Integer, Integer> boardCircuitAmount;
+    private Map<Integer, PositionStatus> boardCircuitStatus;
 
 
     //CONSTRUCTOR
 
-    public Circuit(Map<Integer, Integer> boardCircuit) {
-        this.boardCircuit = boardCircuit;
+    public Circuit(Map<Integer, Integer> boardCircuitAmount, Map<Integer, PositionStatus> boardCircuitStatus) {
+        this.boardCircuitAmount = boardCircuitAmount;
+        this.boardCircuitStatus = boardCircuitStatus;
     }
 
     public Circuit() {
-        this.boardCircuit = new HashMap<>();
+        this.boardCircuitAmount = new HashMap<>();
+        this.boardCircuitStatus = new HashMap<>();
+
         //Circuit initialization
-        initCircuit(this.boardCircuit);
+        initCircuit(this.boardCircuitAmount, this.boardCircuitStatus);
     }
 
     //GETTER AND SETTER
 
 
-    public Map<Integer, Integer> getBoardCircuit() {
-        return boardCircuit;
+    public Map<Integer, Integer> getBoardCircuitAmount() {
+        return boardCircuitAmount;
     }
 
-    public void setBoardCircuit(Map<Integer, Integer> boardCircuit) {
-        this.boardCircuit = boardCircuit;
+    public void setBoardCircuitAmount(Map<Integer, Integer> boardCircuitAmount) {
+        this.boardCircuitAmount = boardCircuitAmount;
     }
 
+    public Map<Integer, PositionStatus> getBoardCircuitStatus() {
+        return boardCircuitStatus;
+    }
 
+    public void setBoardCircuitStatus(Map<Integer, PositionStatus> boardCircuitStatus) {
+        this.boardCircuitStatus = boardCircuitStatus;
+    }
 
     //PUBLIC METHODS
-    public void initCircuit(Map<Integer, Integer> boardCircuit) {
+
+    //Initialization of the board
+    public void initCircuit(Map<Integer, Integer> boardCircuit, Map<Integer, PositionStatus> boardCircuitStatus) {
         for (int i=0; i<24; i++) {
             switch (i){
                 case 0:
                     boardCircuit.put(i, 2);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_1);
+                    break;
+                case 5:
+                    boardCircuit.put(i, 5);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_2);
+                    break;
+                case 7:
+                    boardCircuit.put(i, 3);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_2);
                     break;
                 case 11:
                     boardCircuit.put(i, 5);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_1);
+                    break;
+                case 12:
+                    boardCircuit.put(i, 5);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_2);
                     break;
                 case 16:
                     boardCircuit.put(i, 3);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_1);
                     break;
                 case 18:
                     boardCircuit.put(i, 5);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_1);
+                    break;
+                case 23:
+                    boardCircuit.put(i, 2);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_2);
                     break;
                 default:
                     boardCircuit.put(i, 0);
+                    boardCircuitStatus.put(i, PositionStatus.PLAYER_1);
                     break;
             }
         }
     }
+
 }
--- src/test/java/it/croci/games/backgammon/model/CircuitTest.java
+++ src/test/java/it/croci/games/backgammon/model/CircuitTest.java
@@ -12,27 +12,52 @@ class CircuitTest {
     void initPosition() {
         Circuit circuit = new Circuit();
 
-        Map<Integer, Integer> boardCircuit = circuit.getBoardCircuit();
+        Map<Integer, Integer> boardCircuit = circuit.getBoardCircuitAmount();
 
-        //Test that the token are placed correctly
-        assertEquals(2, (int) boardCircuit.get(0));
-        assertEquals(5, (int) boardCircuit.get(11));
-        assertEquals(3, (int) boardCircuit.get(16));
-        assertEquals(5, (int) boardCircuit.get(18));
+        //Test that the counters are placed correctly
+        //First player
+        assertEquals(2, boardCircuit.get(0));
+        assertEquals(5, boardCircuit.get(11));
+        assertEquals(3, boardCircuit.get(16));
+        assertEquals(5, boardCircuit.get(18));
+        //Second player
+        assertEquals(5, boardCircuit.get(5));
+        assertEquals(3, boardCircuit.get(7));
+        assertEquals(5, boardCircuit.get(12));
+        assertEquals(2, boardCircuit.get(23));
     }
 
     @Test
-    void initTokenAmount() {
+    void initCountersAmount() {
         Circuit circuit = new Circuit();
 
-        Map<Integer, Integer> boardCircuit = circuit.getBoardCircuit();
-        int tokenAmount = 0;
+        Map<Integer, Integer> boardCircuit = circuit.getBoardCircuitAmount();
+        int countersAmount = 0;
         for (Map.Entry<Integer, Integer> entry: boardCircuit.entrySet()) {
-            tokenAmount += entry.getValue();
+            countersAmount += entry.getValue();
         }
 
-        //Test that the token amount is correct (15)
-        assertEquals(15, tokenAmount);
+        //Test that the counter amount is correct (30)
+        assertEquals(30, countersAmount);
     }
 
+    @Test
+    void initPositionStatus() {
+        Circuit circuit = new Circuit();
+
+        Map<Integer, PositionStatus> boardCircuitStatus = circuit.getBoardCircuitStatus();
+
+        //Test that the counters are placed correctly
+        //First player
+        assertEquals(PositionStatus.PLAYER_1, boardCircuitStatus.get(0));
+        assertEquals(PositionStatus.PLAYER_1, boardCircuitStatus.get(11));
+        assertEquals(PositionStatus.PLAYER_1, boardCircuitStatus.get(16));
+        assertEquals(PositionStatus.PLAYER_1, boardCircuitStatus.get(18));
+        //Second player
+        assertEquals(PositionStatus.PLAYER_2, boardCircuitStatus.get(5));
+        assertEquals(PositionStatus.PLAYER_2, boardCircuitStatus.get(7));
+        assertEquals(PositionStatus.PLAYER_2, boardCircuitStatus.get(12));
+        assertEquals(PositionStatus.PLAYER_2, boardCircuitStatus.get(23));
+
+    }
 }
